ebbc4f14a6c3aa5441c24ebb2510487a3df1d4ca
MeherThanmaiee <62166597+MeherThanmaiee@users.noreply.github.com>
2021-04-29 18:50:37 +0530

    Update activity2.c

--- src/activity2.c
+++ src/activity2.c
@@ -16,22 +16,24 @@
  *
  */
 
-void InitADC(){
-    ADMUX = (1<<REFS0);
-    ADCSRA = (1<<ADEN)|(7<<ADPS0);
+void InitADC(){ //Intializtion
+    ADMUX = (1<<REFS0); //Aref=Avcc ,to set resolution
+    ADCSRA = (1<<ADEN)|(7<<ADPS0); //to enable
 
 }
 
-uint16_t ReadADC(uint8_t ch)
+uint16_t ReadADC(uint8_t ch) //ReadADC(0) means channel 0
 {
-    ADMUX&=0xf8;
-    ch = ch&0b00000111;
-    ADMUX|=ch;
+     //select ADC Channel ch must be o to 7
+    
+    ADMUX&=0xf8; // 0xf8= 1111 1000 to clear MUX0,MUX1,MUX2
+    ch = ch&0b00000111; // to enable MUX0,MUX1,MUX2
+    ADMUX|=ch; //to set the channel
 
-    ADCSRA|=(1<<ADSC);
+    ADCSRA|=(1<<ADSC); //start single conversion
 
-    while(!(ADCSRA & (1<<ADIF)));
-    ADCSRA|=(1<<ADIF);
+    while(!(ADCSRA & (1<<ADIF))); //wait for conversion to complete if while(0) it will come out of loop
+    ADCSRA|=(1<<ADIF); //set ADIF by writing 1 to it
     return(ADC);
 
 }
